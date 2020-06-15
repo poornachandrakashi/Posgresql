8ff755d1a2e9d2f12b064e36f17b0553e4c98dd7
poornachandra kashi <44542685+poornachandrakashi@users.noreply.github.com>
2020-06-16 00:13:00 +0530

    jsjnd

--- db.py
+++ db.py
@@ -13,7 +13,26 @@ def inserttable(roll,name,marks):
     conn=sqlite3.connect('lite.db')
     cur=conn.cursor()
     cur.execute("INSERT INTO data VALUES(?,?,?)",(roll,name,marks))
-    cur.commit()
-    cur.close()
+    conn.commit()
+    conn.close()
     
 inserttable(54,"poorna",100)
+
+def display():
+    conn=sqlite3.connect('lite.db')
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM data")
+    row=cur.fetchall()
+    conn.commit()
+    conn.close()
+    return row
+
+print(display())
+
+def delete(roll):
+    conn=sqlite3.connect('lite.db')
+    cur=conn.cursor()
+    cur.execute("DELETE FROM data WHERE rollno=?",(roll,))
+    conn.commit()
+    conn.close()
+delete(54)
--- db2.py
+++ db2.py
@@ -9,4 +9,30 @@ def createtable():
     conn.commit()
     conn.close()
     
-createtable()
+def inserttable(roll,name,marks):
+    conn=psycopg2.connect("dbname='data' user='postgres' password='poorna1999' port='5432' host='localhost' ")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO data VALUES(%s,%s,%s)",(roll,name,marks))
+    conn.commit()
+    conn.close()
+    
+# inserttable(54,"poorna",100)
+# inserttable(44,"Navya",98)
+def display():
+    conn=psycopg2.connect("dbname='data' user='postgres' password='poorna1999' port='5432' host='localhost' ")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM data")
+    row=cur.fetchall()
+    conn.commit()
+    conn.close()
+    return row
+
+print(display())
+
+def delete(roll):
+    conn=psycopg2.connect("dbname='data' user='postgres' password='poorna1999' port='5432' host='localhost' ")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM data WHERE rollno=%s",(roll,))
+    conn.commit()
+    conn.close()
+delete(44)
